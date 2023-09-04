1f3d9fd261bebfcd64705a578176a71dcd4bda00
Yugam Amish Patel <yugampatel@gmail.com>
2023-09-04 16:51:22 -0500

    done:

--- lib/main.dart
+++ lib/main.dart
@@ -34,90 +34,42 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     var city = TextEditingController();
     var country = TextEditingController();
-
+    var dateTime = DateTime.now();
     var arrayNames = ["heet", "yugam", "bimal", "shiv", "viraj", "utsav"];
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        textTheme: const TextTheme(
-          displayMedium: TextStyle(
-              color: Colors.lightBlue,
-              fontSize: 50,
-              fontFamily: 'yugamsfont',
-              fontWeight: FontWeight.bold,
-              letterSpacing: 10,
-              wordSpacing: 10),
-        ),
       ),
       home: Scaffold(
-        backgroundColor: Color.fromARGB(120, 7, 92, 108),
-        appBar: AppBar(
-          title: Text("Flutter"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 300,
-                child: TextField(
-                  controller: city,
-                  decoration: InputDecoration(
-                    prefixIcon: IconButton(
-                      icon: Icon(Icons.email),
-                      onPressed: () {},
-                    ),
-                    hintText: "Email",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                        color: Colors.blue,
-                        width: 4,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                height: 11,
-              ),
-              Container(
-                width: 300,
-                child: TextField(
-                  // keyboardType: TextInputType.number,
-                  controller: country,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    suffixIcon: IconButton(
-                      icon: Icon(Icons.remove_red_eye),
-                      onPressed: () {},
-                    ),
-                    hintText: "Password",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                        color: Colors.green,
-                        width: 4,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                height: 11,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    String cityText = city.text.toString();
-                    String countryText = country.text.toString();
-                    print("Email : $cityText Pass : $countryText");
-                  },
-                  child: Text("Submit"))
-            ],
+          backgroundColor: Color.fromARGB(120, 7, 92, 108),
+          appBar: AppBar(
+            title: Text("Flutter"),
           ),
-        ),
-      ),
+          body: Center(
+            child: Container(
+              width: 300,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'current time : ${dateTime.hour}:${dateTime.minute}:${dateTime.second}',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'yugamsfont',
+                      fontWeight: FontWeight.w300,
+                      color: Colors.red,
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {});
+                      },
+                      child: Text("Submit"))
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
